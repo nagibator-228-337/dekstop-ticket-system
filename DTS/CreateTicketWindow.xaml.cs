9d730b6c7cbb2dc984edac109241bfcb6ecc1fad
nagibator-228-337 <ererewer123@gmail.com>
2026-01-20 15:50:04 +0200

    Initial windows were created. A basic framework for the employee window has also been set up.

---
+++ DTS/CreateTicketWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DTS
+{
+    public partial class CreateTicketWindow : Window
+    {
+        public CreateTicketWindow()
+        {
+            InitializeComponent();
+
+            //placeholder logic
+            SubjectTextBox.GotFocus += (s, e) => UpdatePlacholder(SubjectTextBox, SubjectPlaceHolder);
+            SubjectTextBox.LostFocus += (s, e) => UpdatePlacholder(SubjectTextBox, SubjectPlaceHolder);
+            SubjectTextBox.TextChanged += (s, e) => UpdatePlacholder(SubjectTextBox, SubjectPlaceHolder);
+
+            ThemeTextBox.GotFocus += (s, e) => UpdatePlacholder(ThemeTextBox, ThemeTextPlaceholder);
+            ThemeTextBox.LostFocus += (s, e) => UpdatePlacholder(ThemeTextBox, ThemeTextPlaceholder);
+            ThemeTextBox.TextChanged += (s, e) => UpdatePlacholder(ThemeTextBox, ThemeTextPlaceholder);
+
+            EmailTextBox.GotFocus += (s, e) => UpdatePlacholder(EmailTextBox, EmailPlaceholder);
+            EmailTextBox.LostFocus += (s, e) => UpdatePlacholder(EmailTextBox, EmailPlaceholder);
+            EmailTextBox.TextChanged += (s, e) => UpdatePlacholder(EmailTextBox, EmailPlaceholder);
+        }
+
+        private void UpdatePlacholder (TextBox box, TextBlock placeholder)
+        {
+            if (box.IsFocused || !string.IsNullOrWhiteSpace(box.Text))
+            {
+                placeholder.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                placeholder.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
--- DTS/MainWindow.xaml.cs
+++ DTS/MainWindow.xaml.cs
@@ -17,6 +17,15 @@ namespace DTS
         public MainWindow()
         {
             InitializeComponent();
+            
+            //var window = new CreateTicketWindow();
+            //window.Show(); // или window.ShowDialog();
+
+        }
+
+        private void LoginButton_click( object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new MainEmployeePage());
         }
     }
 }
